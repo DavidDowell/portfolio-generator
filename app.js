0df91d2bb4959862fbceeefdb13040d4413dcec1
DavidDowell <daviddowell1@gmail.com>
2022-09-21 12:27:22 -0400

    use filter and map to separate feature and non feature projects into HTML

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer')
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -30,9 +30,22 @@ const promptUser = () => {
             message: 'Enter your GitHub Username'
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ])
 }
@@ -96,5 +109,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData)
+        const pageHTML = generatePage(name, github);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw err;
+
+          console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
